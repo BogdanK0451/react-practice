084b5bca94eb87efdef1967a90055674e200b97c
BogdanK0451 <zeljio@gmail.com>
2020-03-22 22:21:24 +0100

    finished all features

--- react-weather-forecast/src/Components/Form.js
+++ react-weather-forecast/src/Components/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import roundTo from 'round-to';
 
 const KELVIN=273.15; //kelvin to C
 
@@ -7,32 +8,56 @@ export class Form extends React.Component {
 
   render() {
     return (
+      <>
       <form>
         <div id="search">
-          <input ></input>
+          <input onChange={this.props.onChange} value={this.props.inputValue}></input>
           <button onClick={this.props.onClick}>Check Weather</button>
         </div>
         <div id="result"> 
-          {this.props.fetchedWeather.weather ? <Result fetchedWeather={this.props.fetchedWeather}/> : <Dummy/>}
-          <button>Celsius ℃</button>
-          <button>Fahrenheit ℉</button>
+          {this.props.fetchedWeather.weather ? <Result format={this.props.temperatureFormat} fetchedWeather={this.props.fetchedWeather}/> : <Dummy/>}
         </div>
+        <button type="button" data-format="c" onClick={this.props.handleTemperature}>Celsius ℃</button>
+        <button type="button" data-format="f" onClick={this.props.handleTemperature}>Fahrenheit ℉</button>
       </form>
+      </>
     )
   }
 }
 
 class Result extends React.Component{
+
+  toC(k){
+    return k-KELVIN;  
+  }
+
+  toF(k){
+    return roundTo(this.toC(k)*(9/5)+32,1);
+  }
+
   render(){
-    return(
-      <>
-      <p id="type">Sunny</p>
-      <p id="temperature"> -- ℃</p>
-      <p id="location">New York, US</p>
-      <p id="feels">Feels like {this.props.fetchedWeather.main.feels_like-KELVIN}</p>
-      <p id="min-max">Min {} Max {}</p>
-      </>
-    )
+    if(this.props.format==="c"){
+      return(
+        <>
+        <p id="type">{this.props.fetchedWeather.weather[0].main}</p>
+        <p id="temperature">{roundTo(this.toC(this.props.fetchedWeather.main.temp),1)} ℃</p>
+        <p id="location">{this.props.fetchedWeather.name}, {this.props.fetchedWeather.sys.country}</p>
+        <p id="feels">Feels like {roundTo(this.toC(this.props.fetchedWeather.main.feels_like),1)} ℃</p>
+        <p id="min-max">Min: {roundTo(this.toC(this.props.fetchedWeather.main.temp_min),1)}℃ Max: {roundTo(this.toC(this.props.fetchedWeather.main.temp_max),1)}℃</p>
+        </>
+      )
+    }
+    else if(this.props.format === "f"){
+      return(
+        <>
+        <p id="type">{this.props.fetchedWeather.weather[0].main}</p>
+        <p id="temperature">{roundTo(this.toF(this.props.fetchedWeather.main.temp),1)} °F</p>
+        <p id="location">{this.props.fetchedWeather.name}, {this.props.fetchedWeather.sys.country}</p>
+        <p id="feels">Feels like {roundTo(this.toF(this.props.fetchedWeather.main.feels_like),1)} °F</p>
+        <p id="min-max">Min: {roundTo(this.toF(this.props.fetchedWeather.main.temp_min),1)}°F Max: {roundTo(this.toF(this.props.fetchedWeather.main.temp_max),1)}°F</p>
+        </>
+      )
+    }
   }
 }
 
--- react-weather-forecast/src/index.js
+++ react-weather-forecast/src/index.js
@@ -1,67 +1,137 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../src/index.css';
-import {Form} from '../src/Components/Form'
-import cloneDeep from 'clone-deep'
+import React from "react";
+import ReactDOM from "react-dom";
+import "../src/index.css";
+import { Form } from "../src/Components/Form";
+import cloneDeep from "clone-deep";
 
-
-
-class Weather extends React.Component{
-  constructor(props){
+class Weather extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       myCity: "Belgrade",
-      weather:{}
-    }
+      weather: {},
+      temperatureFormat: "c",
+      backgroundURL: "",
+      loading: true
+    };
 
     this.handleClick = this.handleClick.bind(this);
-    this.l=this.l.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleTemperature = this.handleTemperature.bind(this);
   }
-  
 
-l(){
-  console.log(this.state.weather);
-}
-  
-handleClick(e){
-  e.preventDefault();
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.myCity}&appid=0ba6323855c72753ca4f2f7674b8af5a`,{mode:"cors"})
-  .then(response => response.json()).then(data => this.setState({weather: cloneDeep(data)}))
-  .catch(function(err){
-    console.log(err);
-  });
-}
+  l() {
+    console.log(this.state.weather);
+    console.log(this.state.myCity);
+  }
+  handleChange(e) {
+    this.setState({ myCity: e.target.value });
+  }
 
-componentDidMount(){
-  setInterval(this.l,1000);
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.myCity}&appid=0ba6323855c72753ca4f2f7674b8af5a`,{mode:"cors"})
-  .then(response => response.json()).then(data => this.setState({weather: cloneDeep(data)}))
-  .catch(function(err){
-    console.log(err);
-  });
-}
+  handleTemperature(e) {
+    this.setState({ temperatureFormat: e.target.dataset.format });
+  }
 
-render(){
-  return(
-    <Form onClick={this.handleClick} fetchedWeather={this.state.weather}/>
+  handleClick(e) {
+    this.setState({ loading: true });
+    e.preventDefault();
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${this.state.myCity}&appid=0ba6323855c72753ca4f2f7674b8af5a`,
+      { mode: "cors" }
     )
-}
+      .then(response => response.json())
+      .then(data => {
+        if (data.cod === "404") {
+          window.alert("City with that name doesn't exist");
+        } else {
+          this.setState({ weather: cloneDeep(data) });
 
+          return fetch(
+            `https://source.unsplash.com/1920x1080/?{${this.state.weather.weather[0].main}},{weather}`,
+            { mode: "cors" }
+          )
+            .then(response => response)
+            .then(data => {
+              this.setState({ backgroundURL: data.url });
+              this.setState({ loading: false });
+            })
+            .catch(function(err) {
+              console.log(err);
+            });
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${this.state.myCity}&appid=0ba6323855c72753ca4f2f7674b8af5a`,
+      { mode: "cors" }
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ weather: cloneDeep(data) });
+
+        fetch(
+          `https://source.unsplash.com/1920x1080/?{${this.state.weather.weather[0].main}},{weather}`,
+          {
+            mode: "cors"
+          }
+        )
+          .then(response => response)
+          .then(data => {
+            this.setState({ backgroundURL: data.url });
+            this.setState({ loading: false });
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div
+        id="background"
+        style={{ backgroundImage: `url(${this.state.backgroundURL})` }}
+      >
+        <Form
+          inputValue={this.state.myCity}
+          onChange={this.handleChange}
+          onClick={this.handleClick}
+          handleTemperature={this.handleTemperature}
+          temperatureFormat={this.state.temperatureFormat}
+          fetchedWeather={this.state.weather}
+        />
+
+        <div className={`${this.state.loading === true ? "lds-roller" : ""}`}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <Weather />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+function toCelsius() {}
 
-function toCelsius(){
-
-}
-
-function toFahrenheit(){
-
-}
-
+function toFahrenheit() {}
